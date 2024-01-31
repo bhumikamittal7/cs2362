b52437558f656c9d6f7f367ca0cf5c7aa5ac9633
bhumikamittal7 <bhumika.mittal_ug24@ashoka.edu.in>
2024-01-31 08:28:47 +0530

    Q1A1

--- a1/q1_test.py
+++ a1/q1_test.py
@@ -1,18 +1,3 @@
-'''
-Write a program to implement the Substitute Cipher. Your program must have three separate
-subprograms: [15]
-- KeyGen: This program lets Alice/Bob choose a random permutation π : {0, . . . , 25} →
-{0, . . . , 25} from the set of all such permutations as a common secret key sk.
-- Enc: This program takes as input an english text and outputs its encryption (as described by
-the Substitution Cipher) using the secret key sk = π
-- Dec: This program takes as input a ciphertext and decrypts it (as described by the Substitution
-Cipher) to a english text, using the secret key sk = π.
-'''
-
-#Assumptions:
-#1. User input is a string of english letters only (no spaces, no punctuation, no numbers) - all lowercase
-#2. User input is a string of length 1 or greater
-#======================================================================================================================================================================
 import random
 
 def encoding(userInput):
@@ -26,21 +11,21 @@ def encoding(userInput):
         return encodedInput
     else:
         print("User input must be a string of English letters only (no spaces, no punctuation, no numbers) of length at least 1 - all lowercase")
-        return "Invalid Input"
+        return
 
 def keygen():
-    #permutation Pi: {0, . . . , 25} → {0, . . . , 25}
+    # generate a random permutation of the alphabet
     sk = list(range(26))
     random.shuffle(sk)
     return sk
 
 def enc(encodedInput, sk):
-    # encryption
+    # take in user input and convert to cipher using sk
     cipher = [sk[i] for i in encodedInput]
     return cipher
 
 def dec(cipher, sk):
-    # decryption
+    # take in cipher and convert to user input using sk
     decrypted = [sk.index(i) for i in cipher]
     return decrypted
 
@@ -56,8 +41,6 @@ print("====================================")
 encodedInput = encoding(userInput)
 print("Encoded Input: ", encodedInput)
 print("====================================")
-if encodedInput == "Invalid Input":
-    exit()
 
 sk = keygen()
 print("Secret Key: ", sk)
